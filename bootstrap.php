acf1a6d8ec28e96c7bb317ddf56bbfe5b6a7e31e
GiadaChiar <132461029+GiadaChiar@users.noreply.github.com>
2026-06-20 11:53:31 +0200

    adapting

--- bootstrap.php
+++ bootstrap.php
@@ -6,8 +6,13 @@ use App\Core\Database;
 require __DIR__ . '/vendor/autoload.php';
 
 // ENV
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/');
-$dotenv->load();
+
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
+/*$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/');
+$dotenv->load();*/
 
 // DATABASE (creato UNA volta)
 $database = new Database();
